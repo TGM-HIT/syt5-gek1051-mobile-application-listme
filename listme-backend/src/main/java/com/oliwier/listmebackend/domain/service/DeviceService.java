ab27a71d0fb465bad103c067588667726154eceb
Oliwier Przewlocki <oprzewlocki@student.tgm.ac.at>
2026-04-16 23:58:09 +0200

    fix notifivation error

--- listme-backend/src/main/java/com/oliwier/listmebackend/domain/service/DeviceService.java
+++ listme-backend/src/main/java/com/oliwier/listmebackend/domain/service/DeviceService.java
@@ -1,6 +1,7 @@
 package com.oliwier.listmebackend.domain.service;
 
 import com.oliwier.listmebackend.domain.model.Device;
+import com.oliwier.listmebackend.domain.model.User;
 import com.oliwier.listmebackend.domain.repository.DeviceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,10 +14,16 @@ import java.util.UUID;
 public class DeviceService {
 
     private final DeviceRepository deviceRepository;
+    private final UserService userService;
 
     @Transactional
-    public Device getOrCreate(UUID deviceId) {
-        return deviceRepository.findById(deviceId)
+    public Device getOrCreate(UUID deviceId, UUID userId) {
+        User user = userService.getOrCreate(userId);
+        Device device = deviceRepository.findById(deviceId)
                 .orElseGet(() -> deviceRepository.save(new Device(deviceId)));
+        if (device.getUser() == null || !device.getUser().getId().equals(userId)) {
+            device.setUser(user);
+        }
+        return device;
     }
 }
